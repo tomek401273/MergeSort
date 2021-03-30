97562b447511b978bd42c802c111f952e92f8d68
tomek401273 <tomek371240@gmail.com>
2021-03-30 09:10:07 +0200

    refractoring

--- src/main/java/com/merge/sort/App.java
+++ src/main/java/com/merge/sort/App.java
@@ -1,30 +1,38 @@
 package com.merge.sort;
 
 import com.merge.sort.config.AppConfig;
-import com.merge.sort.file.parser.FileParser;
+import com.merge.sort.file.parser.FileParserPerson;
 import com.merge.sort.file.reader.FileReader;
-import com.merge.sort.file.writer.FileWriter;
-import com.merge.sort.service.MergeSort;
+import com.merge.sort.file.writer.FileWriterPerson;
+import com.merge.sort.model.Person;
+import com.merge.sort.service.ComparatorPerson;
+import com.merge.sort.service.MergeSortPerson;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        AppConfig.setInputFilePath("input/test/input-test1.txt");
+        AppConfig.setInputFilePath("input/test/input-person-list.txt");
         AppConfig.setOutputFilePath("output/test/output-test1.txt");
 
         FileReader fileReader = new FileReader();
         List<String> inputList = fileReader.getInputLines(AppConfig.getInputFilePath());
-        FileParser fileParser = new FileParser();
-        List<Integer[]> result = fileParser.parse(inputList);
-        MergeSort mergeSort = new MergeSort();
-        for (Integer[] line : result) {
-            mergeSort.sort(line);
-            Arrays.stream(line).map(x -> x + ", ").forEach(System.out::print);
-            System.out.println();
-        }
-        FileWriter fileWriter = new FileWriter();
-        fileWriter.write(result, AppConfig.getOutputPath());
+        inputList.forEach(s -> System.out.println(s));
+        FileParserPerson fileParserPerson= new FileParserPerson();
+        List<Person> people= fileParserPerson.parse(inputList);
+        MergeSortPerson mergeSortPerson = new MergeSortPerson();
+        Map<String, ComparatorPerson> map =new HashMap<>();
+        map.put("age asc", (person1, person2) -> person1.getAge().compareTo(person2.getAge())<0);
+        map.put("name asc", (person1, person2) -> person1.getName().compareTo(person2.getName()) < 0);
+        ComparatorPerson comparatorPerson = map.get("age asc") ;
+//        ComparatorPerson comparatorPerson = map.get("name asc") ;
+
+        mergeSortPerson.sort(people, comparatorPerson);
+        people.forEach(person -> System.out.println(person));
+
+        FileWriterPerson fileWriterPerson =new FileWriterPerson();
+        fileWriterPerson.write(people, AppConfig.getOutputPath());
     }
 }
--- src/test/java/com/merge/sort/parser/FileParserTest.java
+++ src/test/java/com/merge/sort/parser/FileParserTest.java
@@ -1,24 +1,12 @@
 package com.merge.sort.parser;
 
-import com.merge.sort.file.parser.FileParser;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 class FileParserTest {
 
     @Test
     void parse() {
         // given
-        List<String> inputList = new ArrayList<>();
-        inputList.add("1 20 5 10 12 14 16 25");
-        inputList.add("2 5 10 22 40 1 7 15");
-        inputList.add("12 101 1 12");
-        // when
-        FileParser fileParser= new FileParser();
-        List<Integer[]> result = fileParser.parse(inputList);
-        System.out.println(result);
         // then
     }
 }
